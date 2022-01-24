6b4153d3c5ba6e1bf40f487209da652c34f89bc4
Luismkm <luismarcelo13@yahoo.com.br>
2022-01-23 22:00:56 -0300

    refactor: add generic params to HttpPostClient

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -4,15 +4,17 @@ import { mockAuthentication } from '@/domain/test/mock-authentication';
 import { HttpStatusCode } from '@/data/protocols/http/http-response';
 import { InvalidCredentialsError } from '@/domain/model/errors/invalid-credentials-error';
 import { UnexpectedError } from '@/domain/model/errors/unexpected-error';
+import { AuthenticationParams } from '@/domain/usecases/authentication';
+import { AccountModel } from '@/domain/model/account-model';
 import { RemoteAuthentication } from './remote-authentication';
 
 type SutTypes = {
   sut: RemoteAuthentication
-  httpPostClientSpy: HttpPostClientSpy
+  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy();
+  const httpPostClientSpy = new HttpPostClientSpy<AuthenticationParams, AccountModel>();
   const sut = new RemoteAuthentication(url, httpPostClientSpy);
   return {
     sut,
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -3,11 +3,12 @@ import { IHttpPostClient } from '@/data/protocols/http/http-post-client';
 import { HttpStatusCode } from '@/data/protocols/http/http-response';
 import { InvalidCredentialsError } from '@/domain/model/errors/invalid-credentials-error';
 import { UnexpectedError } from '@/domain/model/errors/unexpected-error';
+import { AccountModel } from '@/domain/model/account-model';
 
 export class RemoteAuthentication {
   constructor(
     private readonly url: string,
-    private readonly httpPostClient: IHttpPostClient,
+    private readonly httpPostClient: IHttpPostClient<AuthenticationParams, AccountModel>,
   ) {}
 
   async auth(params: AuthenticationParams): Promise<void> {
