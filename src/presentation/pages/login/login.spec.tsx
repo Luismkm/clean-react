9e7587d88ba566b4b5cd706dcf58997fc377f7bb
Luismkm <luismarcelo13@yahoo.com.br>
2023-04-07 10:07:03 -0300

    test: ensure Login present error and ride spinner on authentication failure

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,8 +1,9 @@
 import faker from 'faker';
 import {
-  render, RenderResult, fireEvent, cleanup,
+  render, RenderResult, fireEvent, cleanup, waitFor,
 } from '@testing-library/react';
 import { ValidationStub, AuthenticationSpy } from '@/presentation/test';
+import { InvalidCredentialsError } from '@/domain/errors';
 import Login from './login';
 
 type SutTypes = {
@@ -126,5 +127,17 @@ describe('Login Component', () => {
     populateEmailField(sut);
     fireEvent.submit(sut.getByTestId('form'));
     expect(authenticationSpy.callsCount).toBe(0);
+  });
+
+  test('Should present error if Authentication fails', async () => {
+    const { sut, authenticationSpy } = makeSut();
+    const error = new InvalidCredentialsError();
+    jest.spyOn(authenticationSpy, 'auth').mockReturnValueOnce(Promise.reject(error));
+    simulateValidSubmit(sut);
+    const errorWrap = sut.getByTestId('error-wrap');
+    await waitFor(() => errorWrap);
+    const mainError = sut.getByTestId('main-error');
+    expect(mainError.textContent).toBe(error.message);
+    expect(errorWrap.childElementCount).toBe(1);
   });
 });
