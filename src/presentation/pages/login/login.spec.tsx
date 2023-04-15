e2619d2996768779315d719abc8d7f0aac63add5
Luismkm <luismarcelo13@yahoo.com.br>
2023-04-14 21:43:51 -0300

    refactor: alter Login import

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -7,7 +7,7 @@ import {
 } from '@testing-library/react';
 import { ValidationStub, AuthenticationSpy } from '@/presentation/test';
 import { InvalidCredentialsError } from '@/domain/errors';
-import Login from './login';
+import { Login } from '@/presentation/pages';
 
 type SutTypes = {
   sut: RenderResult;
