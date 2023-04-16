ea7cc1143230e01c3a2e347cb29f8a999e32f714
Luismkm <luismarcelo13@yahoo.com.br>
2023-04-16 20:31:05 -0300

    feat: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 400

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -1,7 +1,7 @@
 import { AccountModel } from '@/domain/model';
 import { AddAccount, AddAccountParams } from '@/domain/usecases';
 import { HttpStatusCode, IHttpPostClient } from '@/data/protocols/http';
-import { EmailInUseError } from '@/domain/errors';
+import { EmailInUseError, UnexpectedError } from '@/domain/errors';
 
 export class RemoteAddAccount implements AddAccount {
   constructor(
@@ -15,8 +15,9 @@ export class RemoteAddAccount implements AddAccount {
       body: params,
     });
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: return null;
       case HttpStatusCode.forbiden: throw new EmailInUseError();
-      default: return null;
+      default: throw new UnexpectedError();
     }
   }
 }
