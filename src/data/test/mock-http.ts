bd91cce3cacb99a19aa7c0092678b14db22f93f9
Luismkm <luismarcelo13@yahoo.com.br>
2023-04-16 19:36:37 -0300

    refactor: rename data mocks

--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,6 +1,12 @@
+import * as faker from 'faker';
 import {
   HttpPostParams, IHttpPostClient, HttpResponse, HttpStatusCode,
 } from '@/data/protocols/http';
+
+export const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.random.objectElement(),
+});
 
 export class HttpPostClientSpy<T, R> implements IHttpPostClient<T, R> {
   url?: string;
