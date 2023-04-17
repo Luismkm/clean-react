d71cb58e0d13c7018524ec74ee5fb50e02c5787c
Luismkm <luismarcelo13@yahoo.com.br>
2023-04-17 20:52:53 -0300

    test: ensure SignUp starts with initial state

---
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -0,0 +1,44 @@
+import { RenderResult, render } from '@testing-library/react';
+import SignUp from './signup';
+
+type SutTypes = {
+  sut: RenderResult;
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(
+    <SignUp />,
+  );
+  return {
+    sut,
+  };
+};
+
+const testChildCount = (sut: RenderResult, fieldName: string, count: number): void => {
+  const el = sut.getByTestId(fieldName);
+  expect(el.childElementCount).toBe(count);
+};
+
+const testButtonIsDisabled = (sut: RenderResult, fieldName: string, isDisabled: boolean): void => {
+  const button = sut.getByTestId(fieldName) as HTMLButtonElement;
+  expect(button.disabled).toBe(isDisabled);
+};
+
+const testStatusForFiled = (sut: RenderResult, fieldName: string, validationError?: string): void => {
+  const fieldStatus = sut.getByTestId(`${fieldName}-status`);
+  expect(fieldStatus.title).toBe(validationError || 'Tudo certo!');
+  expect(fieldStatus.textContent).toBe(validationError ? '🔴' : '🟢');
+};
+
+describe('Signup Component', () => {
+  test('should start with initial state', () => {
+    const validationError = 'Campo obrigatório';
+    const { sut } = makeSut();
+    testChildCount(sut, 'error-wrap', 0);
+    testButtonIsDisabled(sut, 'submit', true);
+    testStatusForFiled(sut, 'name', validationError);
+    testStatusForFiled(sut, 'email', validationError);
+    testStatusForFiled(sut, 'password', validationError);
+    testStatusForFiled(sut, 'passwordConfirmation', validationError);
+  });
+});
