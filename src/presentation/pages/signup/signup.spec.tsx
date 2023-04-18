c199c6630dd200e472f02cdb5c87528c7a1b5296
Luismkm <luismarcelo13@yahoo.com.br>
2023-04-17 21:50:13 -0300

    test: ensure SignUp change name status to error if validation dails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,14 +1,25 @@
-import { RenderResult, render } from '@testing-library/react';
-import { Helper } from '@/presentation/test';
+import faker from 'faker';
+import {
+  RenderResult, render, cleanup,
+} from '@testing-library/react';
+import { Helper, ValidationStub } from '@/presentation/test';
 import SignUp from './signup';
 
 type SutTypes = {
   sut: RenderResult;
 }
 
-const makeSut = (): SutTypes => {
+type SutParams = {
+  validationError: string;
+}
+
+const makeSut = (params?: SutParams): SutTypes => {
+  const validationStub = new ValidationStub();
+  validationStub.errorMessage = params?.validationError;
   const sut = render(
-    <SignUp />,
+    <SignUp
+      validation={validationStub}
+    />,
   );
   return {
     sut,
@@ -16,14 +27,23 @@ const makeSut = (): SutTypes => {
 };
 
 describe('Signup Component', () => {
+  afterEach(cleanup);
+
   test('should start with initial state', () => {
-    const validationError = 'Campo obrigatório';
-    const { sut } = makeSut();
+    const validationError = faker.random.words();
+    const { sut } = makeSut({ validationError });
     Helper.testChildCount(sut, 'error-wrap', 0);
     Helper.testButtonIsDisabled(sut, 'submit', true);
     Helper.testStatusForFiled(sut, 'name', validationError);
-    Helper.testStatusForFiled(sut, 'email', validationError);
-    Helper.testStatusForFiled(sut, 'password', validationError);
-    Helper.testStatusForFiled(sut, 'passwordConfirmation', validationError);
+    Helper.testStatusForFiled(sut, 'email', 'Campo obrigatório');
+    Helper.testStatusForFiled(sut, 'password', 'Campo obrigatório');
+    Helper.testStatusForFiled(sut, 'passwordConfirmation', 'Campo obrigatório');
+  });
+
+  test('Should show email error if Validation fails', () => {
+    const validationError = faker.random.words();
+    const { sut } = makeSut({ validationError });
+    Helper.populateField(sut, 'name');
+    Helper.testStatusForFiled(sut, 'name', validationError);
   });
 });
