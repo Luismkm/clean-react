d07117fbc77330564b0944a26ff0812810a44de2
Luismkm <luismarcelo13@yahoo.com.br>
2023-04-19 19:29:22 -0300

    feat: add SignUp Composition

---
+++ src/main/factories/usecases/add-account/remote-add-account--factory.ts
@@ -0,0 +1,5 @@
+import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory';
+import { AddAccount } from '@/domain/usecases';
+import { RemoteAddAccount } from '@/data/usecases/add-account/remote-add-account';
+
+export const makeRemoteAddAccount = (): AddAccount => new RemoteAddAccount('/signup', makeAxiosHttpClient());
