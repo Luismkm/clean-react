93cdd6fbe5bf068b580ec8962b64de7f8e9721b1
Luismkm <luismarcelo13@yahoo.com.br>
2023-04-18 22:35:41 -0300

    refactor: change Validation to receive an object instead of value

--- src/main/factories/pages/login/login-validation-factory.ts
+++ src/main/factories/pages/login/login-validation-factory.ts
@@ -1,7 +1,7 @@
 import { ValidationComposite } from '@/validation/validators';
-import { ValidationBuilder } from '@/validation/builder/validation-builder';
+import { ValidationBuilder as Builder } from '@/validation/builder/validation-builder';
 
 export const makeLoginValidation = (): ValidationComposite => ValidationComposite.build([
-  ...ValidationBuilder.field('email').required().email().build(),
-  ...ValidationBuilder.field('password').required().min(5).build(),
+  ...Builder.field('email').required().email().build(),
+  ...Builder.field('password').required().min(5).build(),
 ]);
